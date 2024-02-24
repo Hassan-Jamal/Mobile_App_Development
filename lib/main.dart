d8632d45dd141587d2d9b36d3c7a72f557c37016
Hassan-Jamal <hassanjamal6577987@gmail.com>
2024-02-24 19:59:00 +0500

    Functions, Classes, Routes, Theme & Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:firstproject/home_page.dart';
+import 'package:firstproject/pages/home_page.dart';
+import 'package:firstproject/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+     // home: HomePage(),  //default route but now we add "/" for home
+      themeMode: ThemeMode.dark, 
+      darkTheme: ThemeData(brightness: Brightness.light),
+      initialRoute: "/home",
+      routes: {
+        "/":(context)=>LoginPage(),
+        "/home":(context)=>HomePage(),
+        "/login":(context)=>LoginPage(),
+      },
     );
   }
 }
