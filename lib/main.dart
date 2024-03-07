d0b47658a3d8f5df2828e3e843724c9873eacc80
Hassan-Jamal <hassanjamal6577987@gmail.com>
2024-03-07 15:03:54 +0500

    Material Drawer, DevTools, ListView, NetworkImage

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     // debugShowCheckedModeBanner: false,  
-       home: LoginPage(),  //default route but now we add "/" for home
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(), //default route but now we add "/" for home
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData(
         brightness: Brightness.light,
